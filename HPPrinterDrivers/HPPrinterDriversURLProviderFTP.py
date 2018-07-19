13e349d44d178768a0971fdded02b9e79e35d050
AutoPKG <johnmcl1@gmail.com>
2018-07-19 12:37:09 -0400

    hp printer drivers - ftp
    Version of HP URL provider using ftplib for direct interaction.

---
+++ HPPrinterDrivers/HPPrinterDriversURLProviderFTP.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#
+#
+# Copyright 2018 John McLaughlin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import re
+import urllib2
+import os
+import ftplib
+import fnmatch
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["HPPrinterDriversURLProviderFTP"]
+
+update_url = ("ftp://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Installations/Essentials")
+
+
+class HPPrinterDriversURLProviderFTP(Processor):
+    description = "Provides URL to the latest HP printer driver releases."
+    input_variables = {
+        "url": {
+            "required": False,
+            "description": ("Override URL. If provided, this processor "
+                "just returns without doing anything."),
+        },
+    }
+    output_variables = {
+        "url": {
+            "description": "URL to the latest HP printer driver releases.",
+        },
+    }
+    
+    __doc__ = description
+    
+
+    def get_hp_url(self):
+	'''Read in URL'''
+	try:
+	    f = urllib2.urlopen(update_url)
+	    url_data = f.read()
+	    f.close()
+	except BaseException as e:
+            raise ProcessorError(
+                "Can't download %s: %s" % (update_url, e))	
+
+	'''Find all installers in URL'''
+	pkg = ''
+	pkg_string='.pkg'
+	download_pkg_urls = []
+	for pkg in ftp.nlst(pkg_string):
+	    '''Print pkg'''
+	    if (pkg.endswith('.pkg')):
+	        print 'This download link contains a valid package: %s' % pkg
+		download_pkg_urls.append(pkg)
+ 	return download_pkg_urls
+
+    def main(self):
+	'''Return a list of download URLs for latest HP printer drivers'''
+        if "url" in self.env:
+            self.output("Using input URL %s" % self.env["url"])
+            return
+        self.env["url"] = self.get_hp_url()
+        self.output("Found URL %s" % self.env["url"])
+    
+
+if __name__ == '__main__':
+    processor = HPPrinterDriversURLProviderFTP()
+    processor.execute_shell()
+
