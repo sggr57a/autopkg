f6c88cd3b994b017b5c7173c7236718feb36525e
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-01 19:42:49 -0500

    Set if statement to look if jss_repo_updated exists

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -84,7 +84,7 @@ class Slacker(Processor):
         jss_importer_summary_result = self.env.get("jss_importer_summary_result")
         webhook_url = self.env.get("webhook_url")
 
-        if jss_repo_updated is not None:
+        if jss_repo_updated:
             jss_policy_name = "%s" % jss_importer_summary_result["data"]["Policy"]
             jss_policy_version = "%s" % jss_importer_summary_result["data"]["Version"]
             jss_uploaded_package = "%s" % jss_importer_summary_result["data"]["Package"]
