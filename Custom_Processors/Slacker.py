af82c4cdf49fcf8f70ae9108503320a168af807b
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-04 11:50:08 -0500

    Looking self.get_report_string([jss_package_added]) is not ''

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -89,7 +89,7 @@ class Slacker(Processor):
             print "Category: %s" % category
             print "Package: %s" % jss_uploaded_package
 
-            if jss_importer_summary_result["data"]["jss_package_added"] is not "":
+            if self.get_report_string(["jss_package_added"]) is not "":
 		 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, jss_uploaded_package)
 	    else:
 		 slack_text = "*No new items have been added to the JSS*\nURL: %s\nTitle: %s" % (JSS_URL, prod_name)
