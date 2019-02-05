0e858621c0bb8e243e5fa8a1a7d1b10d895ee3f8
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-04 19:15:49 -0500

    Counting if jss_uploaded_package.count(Package) > 2.

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -72,10 +72,11 @@ class Slacker(Processor):
         prod_name = self.env.get("prod_name")
 	jss_importer_summary_result = self.env.get("jss_importer_summary_result")
 	jss_changed_objects = self.env.get("jss_changed_objects")
+	jss_package_updated = self.env.get("jss_package_updated")
 	webhook_url = self.env.get("webhook_url")
 	
 	print "CHANGED OBJECTS: %s" % jss_changed_objects
-
+	
 	if jss_changed_objects:
             jss_policy_name = "%s" % jss_importer_summary_result["data"]["Policy"]
             jss_policy_version = "%s" % jss_importer_summary_result["data"]["Version"]
@@ -86,7 +87,7 @@ class Slacker(Processor):
             print "Category: %s" % category
             print "Package: %s" % jss_uploaded_package
 
-            if jss_uploaded_package:
+            if jss_uploaded_package.count(Package) > 2:
 		 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, jss_uploaded_package)
 	    else:
 		 slack_text = "*No new items have been added to the JSS*\nURL: %s\nTitle: %s" % (JSS_URL, prod_name)
