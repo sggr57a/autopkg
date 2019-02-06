070fc8a551a821f9bbb1c2a52434323f4db9faa2
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-06 18:41:10 -0500

    Defined global name to reflect package for package name.

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -81,7 +81,7 @@ class Slacker(Processor):
 	webhook_url = self.env.get("webhook_url")
 	
 	print "CHANGES: %s" % jss_changed_objects 
-	print "PACKAGE: %s" % jss_repo_updated
+	print "PACKAGE: %s" % package
 
 	if jss_changed_objects["jss_repo_updated"]:
             jss_policy_name = "%s" % jss_importer_summary_result["data"]["Policy"]
@@ -91,10 +91,10 @@ class Slacker(Processor):
             print "Title: %s" % prod_name
             print "Version: %s" % jss_policy_version
             print "Category: %s" % category
-            print "Package: %s" % jss_repo_updated
+            print "Package: %s" % package
 
             if jss_uploaded_package:
-		 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, jss_repo_updated)
+		 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, package)
 
             slack_data = {'text': slack_text}
 
