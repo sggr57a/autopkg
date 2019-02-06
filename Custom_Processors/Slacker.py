b531e57771916a2e673fd5915c284c97231ad2a6
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-06 17:16:02 -0500

    Added jss_uploaded_package back to check and then post.

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -85,7 +85,7 @@ class Slacker(Processor):
             print "Title: %s" % prod_name
             print "Version: %s" % jss_policy_version
             print "Category: %s" % category
-            print "Package: %s" % jss_uploaded_added
+            print "Package: %s" % jss_uploaded_package
 
             if jss_uploaded_package:
 		 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, jss_uploaded_package)
