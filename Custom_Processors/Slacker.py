71e288199f9edac45441f909e67a1749847781c2
John McLaughlin <jmclaughlin@ECTR-IT-24860.local>
2019-02-06 20:39:41 -0500

    Removed package name variables.

--- Custom_Processors/Slacker.py
+++ Custom_Processors/Slacker.py
@@ -83,10 +83,8 @@ class Slacker(Processor):
             print "Title: %s" % prod_name
             print "Version: %s" % jss_policy_version
             print "Category: %s" % category
-#            print "Package: %s" % jss_uploaded_package
 
             if jss_uploaded_package:
-#                slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category, jss_uploaded_package)
                 slack_text = "*New Item added to JSS*\nURL: %s\nTitle: *%s*\nVersion: *%s*\nCategory: *%s*\nUploaded Package Name: *%s*" % (JSS_URL, prod_name, jss_policy_version, category)
 
             slack_data = {'text': slack_text}
